8131d35d6f8cce2bde2bc6de58184542543306b7
Ayush Singh <ayush@beagleboard.org>
2026-03-05 18:25:39 +0530

    boards: beagle: Add zepto support
    Initial zepto support
    
    Signed-off-by: Ayush Singh <ayush@beagleboard.org>

---
+++ boards/beagle/zepto/board.cmake
@@ -0,0 +1,11 @@
+# BeagleBoard.org PocketBeagle 2
+#
+# Copyright (c) 2026 BeagleBoard.org Foundation
+#
+# SPDX-License-Identifier: Apache-2.0
+
+if(CONFIG_SOC_MSPM0L1117)
+  board_runner_args(openocd "--no-init" "--no-halt" "--no-targets" "--gdb-client-port=3339"
+  "--config=${BOARD_DIR}/support/openocd.cfg")
+  include(${ZEPHYR_BASE}/boards/common/openocd.board.cmake)
+endif()
